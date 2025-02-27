11ea853be6a21030be79a1b26505f95417fc94e7
chan0yeol <oco723@naver.com>
2025-02-27 16:42:44 +0900

    feat: 문서양식 등록, 조회, 상세조회 기능 추가

--- GroupWare/src/main/java/com/giga/gw/controller/ApprovalController.java
+++ GroupWare/src/main/java/com/giga/gw/controller/ApprovalController.java
@@ -7,16 +7,20 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.giga.gw.dto.ApprovalCategoryDto;
+import com.giga.gw.dto.ApprovalFormDto;
 import com.giga.gw.repository.IApprovalCategoryDao;
 import com.giga.gw.repository.IApprovalDao;
 import com.giga.gw.repository.IEmployeeDao;
 import com.giga.gw.service.IApprovalCategoryService;
+import com.giga.gw.service.IApprovalFormService;
 import com.google.gson.Gson;
 
 import lombok.RequiredArgsConstructor;
@@ -29,7 +33,9 @@ import lombok.extern.slf4j.Slf4j;
 public class ApprovalController {
 	private final IApprovalDao approvalDao;
 	private final IEmployeeDao employeeDao;
-	private final IApprovalCategoryService categoryService;
+	private final IApprovalCategoryService approvalCategoryService;
+	private final IApprovalFormService approvalFormService;
+	
 	@GetMapping("/index.do")
 	public String apprIndex() {
 		return "approval";
@@ -81,7 +87,7 @@ public class ApprovalController {
 	}
 	
 	
-	// TODO 001 전자결재 - 카테고리 Controller
+	// TODO 00100 전자결재 - 카테고리 Controller
 	@GetMapping("/categoryForm.do")
 	public String categoryForm() {
 		return "approvalCategoryForm";
@@ -91,7 +97,7 @@ public class ApprovalController {
 	@GetMapping("/categoryCheck.do")
 	@ResponseBody
 	public boolean categoryCheck(String yname) {
-		return categoryService.categoryCheck(yname.toUpperCase()) == 0 ? true:false;
+		return approvalCategoryService.categoryCheck(yname.toUpperCase()) == 0 ? true:false;
 	}
 	
 	// 카테고리 저장
@@ -100,15 +106,53 @@ public class ApprovalController {
 	public boolean categorySave(@RequestBody ApprovalCategoryDto categoryDto) {
 		System.out.println(categoryDto);
 		categoryDto.setCategory_yname(categoryDto.getCategory_yname().toUpperCase());
-		return categoryService.categoryInsert(categoryDto) == 1 ? true:false;
+		return approvalCategoryService.categoryInsert(categoryDto) == 1 ? true:false;
 	}
 	
 	@GetMapping("/category.do")
 	public String categoryList(Model model) {
-		List<ApprovalCategoryDto> categoryList = categoryService.categorySelect();
+		List<ApprovalCategoryDto> categoryList = approvalCategoryService.categorySelect();
 		model.addAttribute("categoryList", categoryList);
 		return "approvalCategoryList";
 	}
 	
+	// 문서양식 등록시 카테고리 선택을 위한 팝업창
+	@GetMapping("/categoryPop.do")
+	public String categoryPop(Model model) {
+		List<ApprovalCategoryDto> categoryList = approvalCategoryService.categorySelect();
+		model.addAttribute("categoryList", categoryList);
+		return "categoryPop";
+	}
 	
+	// TODO 00101 전자결재 문서양식 Controller
+	// 문서양식 리스트 조회
+	@GetMapping("/approvalFormList.do")
+	public String approvalForm(Model model) {
+		List<ApprovalFormDto> formList = approvalFormService.formSelectAll();
+		model.addAttribute("formList", formList);
+		return "approvalFormList";
+	}
+	
+	@GetMapping("/approvalFormDetail/{id}.do")
+	public String approvalFormDetail(@PathVariable String id, Model model) {
+		System.out.println(id);
+		ApprovalFormDto form = approvalFormService.formSelectDetail(id);
+		model.addAttribute("form",form);
+		return "approvalFormDetail";
+	}
+	
+	// 문서양식 등록 페이지로 이동
+	@GetMapping("/approvalFormCreate.do")
+	public String approvalFormCarete() {
+		return "approvalFormCreate";
+	}
+	
+	@PostMapping("/approvalFormSave.do")
+	@ResponseBody
+	public boolean approvalFormSave(@RequestBody ApprovalFormDto approvalFormDto) {
+		System.out.println(approvalFormDto);
+		int row = approvalFormService.formInsert(approvalFormDto); 
+		return row == 1 ? true:false;
+	}
+		
 }
