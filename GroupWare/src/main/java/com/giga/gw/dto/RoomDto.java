522e9fcc5fc75d3e1fa16647f3305119ff9ad896
wjddbfl3895 <wjddbfl3895@naver.com>
2025-02-27 17:45:52 +0900

    feat : 예약 기능 작업중

--- GroupWare/src/main/java/com/giga/gw/dto/RoomDto.java
+++ GroupWare/src/main/java/com/giga/gw/dto/RoomDto.java
@@ -2,4 +2,13 @@ package com.giga.gw.dto;
 
 public class RoomDto {
 
+//	room_id
+//	room_name
+//	capacity
+//	open_hours
+//	close_hours
+//	created_at
+//	updated_at
+//	use_yn
+//	image_url;
 }
--- GroupWare/src/main/java/com/giga/gw/repository/ReservationDaoImpl.java
+++ GroupWare/src/main/java/com/giga/gw/repository/ReservationDaoImpl.java
@@ -1,5 +1,24 @@
 package com.giga.gw.repository;
 
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.giga.gw.dto.ReservationDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
 public class ReservationDaoImpl implements IReservationDao{
+	
+	private final SqlSessionTemplate sessionTemplate;
+	private final String NS = "com.giga.gw.repository.ReservationDaoImpl.";
+
+	@Override
+	public ReservationDto reservation(Map<String, Object> map) {
+		return sessionTemplate.selectOne(NS+"reservation", map);
+	}
 
 }
